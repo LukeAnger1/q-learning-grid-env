ad36ca660aec5e2a727b97219016cf154a337817
Luke Anger <anger.luke2004@gmail.com>
2024-12-20 09:47:12 -0500

    forgot to commit the utils, should do a checkout to fix commit history but nah

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import pickle
 
 def initialize_q_table(state_space, action_space):
     Qtable = np.zeros((state_space, action_space))
@@ -17,3 +18,16 @@ def epsilon_greedy_policy(Qtable, env, state, epsilon):
         action = env.action_space.sample()
 
     return action
+
+# Function to save Q-table to a file
+def save_qtable(filename, qtable):
+    with open(filename, 'wb') as f:
+        pickle.dump(qtable, f)
+    print(f"Q-table saved to {filename}")
+
+# Function to load Q-table from a file
+def load_qtable(filename):
+    with open(filename, 'rb') as f:
+        qtable = pickle.load(f)
+    print(f"Q-table loaded from {filename}")
+    return qtable
